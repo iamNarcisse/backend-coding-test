bc51909356e45466cb7b32c24202f91e72fb1975
Narcisse Egonu <mechikaegon@gmail.com>
2021-12-19 09:34:19 +0100

    chore:typescript setup

--- dist/pricing_algo.js
+++ dist/pricing_algo.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 1. Write a pricing algorithm function that takes in a start DateTime, end DateTime, price per hour and returns the total price for the duration.
 2. The pricing algorithm should be able to handle the following :
@@ -10,34 +11,21 @@
 6. You may use the library of your choice for date manipulation but DayJS is recommended
 7. Use `ts-node pricing_algo` to run the pricing algorithm
 */
-
-interface OverwritePrice {
-  startDateTime: Date;
-  endDateTime: Date;
-  pricePerHour: number;
-}
-
-const pricingAlgo = (
-  startDateTime: Date,
-  endDateTime: Date,
-  pricePerHour: number,
-  overwritePrice?: OverwritePrice[]
-): number => {
-  let price = 0;
-
-  console.log(price);
-  return price;
+const pricingAlgo = (startDateTime, endDateTime, pricePerHour, overwritePrice) => {
+    let price = 0;
+    console.log(price);
+    return price;
 };
-
 pricingAlgo(new Date('2021-11-13T09:24:00'), new Date('2021-11-15T15:13:00'), 13, [
-  {
-    startDateTime: new Date('2021-11-14T12:00:00'),
-    endDateTime: new Date('2021-11-14T14:01:00'),
-    pricePerHour: 5,
-  },
-  {
-    startDateTime: new Date('2021-11-15T09:00:00'),
-    endDateTime: new Date('2021-11-17T00:00:00'),
-    pricePerHour: 15.2,
-  },
+    {
+        startDateTime: new Date('2021-11-14T12:00:00'),
+        endDateTime: new Date('2021-11-14T14:01:00'),
+        pricePerHour: 5,
+    },
+    {
+        startDateTime: new Date('2021-11-15T09:00:00'),
+        endDateTime: new Date('2021-11-17T00:00:00'),
+        pricePerHour: 15.2,
+    },
 ]);
+//# sourceMappingURL=pricing_algo.js.map
