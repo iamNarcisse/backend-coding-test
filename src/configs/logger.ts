e240381e0a3adfa261df0d8546bf41a96c31d1f1
Narcisse Egonu <mechikaegon@gmail.com>
2021-12-22 00:14:08 +0100

    .

--- src/configs/logger.ts
+++
@@ -1,8 +0,0 @@
-import Sentry, { captureException } from '@sentry/serverless';
-
-// Sentry.AWSLambda.init({
-//   dsn: process.env.SENTRY_DSN as string,
-//   tracesSampleRate: 1.0,
-// });
-
-export { captureException };
